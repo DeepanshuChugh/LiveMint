b242c637bd56d0937a0a122f571481c4639d0df4
ayaznoori <ayaz.noori15@gmail.com>
2022-05-05 19:30:48 +0530

    carousel added

---
+++ components/carousel.js
@@ -0,0 +1,21 @@
+function carouseldiv(){
+    return `<div id="carousel_title"><h2>FROM THE WALL STREET JOURNAL<h2> <span> View All > </span></div>
+    <div id="Carousel_container">
+    <button id="left" onclick="hello()">></button>
+    
+    <div id="icontainer">
+      
+        <div><img src="./components/img/1.png" alt=""> </div>
+        <div><img src="./components/img/2.png" alt=""></div>
+        <div><img src="./components/img/3.png" alt=""></div>
+        <div><img src="./components/img/4.png" alt=""></div>
+        <div><img src="./components/img/5.png" alt=""></h2></div>
+        <div><img src="./components/img/6.png" alt=""></div>
+        <div><img src="./components/img/7.png" alt=""></div>
+        <div><img src="./components/img/8.png" alt=""></div>
+        <div><img src="./components/img/1.png" alt=""></div>
+    </div>
+    <button id="right" onclick="hello2()"> < </button>
+</div>`
+}
+export { carouseldiv }
